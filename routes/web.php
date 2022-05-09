e8e6cf08bd890c32c6671cd72f4b9e3c6e6dd51e
wizard2000 <zb.sina@gmail.com>
2022-05-09 10:22:52 +0430

    add client and admin templfate

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('client.home');
+});
+Route::get('/ts-admin', function () {
+    return view('admin.home');
 });
